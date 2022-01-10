4997ae7e79d97385819fbf7f2e9419acd906f732
Techon360 <vimalpanchal010@gmail.com>
2022-01-10 13:28:30 +0530

    minor changes

--- src/main/java/resources/ExtentReporterNG.java
+++ src/main/java/resources/ExtentReporterNG.java
@@ -11,12 +11,12 @@ public class ExtentReporterNG {
 		
 		String path =System.getProperty("user.dir")+"\\reports\\index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
-		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setReportName("WebNetram Automation Results");
 		reporter.config().setDocumentTitle("Test Results");
 		
-		 extent =new ExtentReports();
+		extent =new ExtentReports();
 		extent.attachReporter(reporter);
-		extent.setSystemInfo("Tester", "Rahul Shetty");
+		extent.setSystemInfo("Tester", "Vimal Panchal");
 		return extent;
 		
 	}
